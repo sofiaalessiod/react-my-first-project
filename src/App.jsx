138b76a2250e3df64176e4da58cf3cdda3c01475
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-08-05 12:42:49 +0200

    Update page title to "Users" in App component

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main className="app">
-      <h1>My First React App</h1>
+      <h1>Users</h1>
 
       <section className="grid">
         {users.map(user => (
