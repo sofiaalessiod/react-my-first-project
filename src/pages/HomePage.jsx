670491ba82b542b151a305eabff9c4d86e500b04
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-08-13 12:51:50 +0200

    chore: Update HomePage to fetch users from API if not found in local storage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,18 +5,32 @@ export default function HomePage() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const data = localStorage.getItem("users"); // get data from local storage
-    const usersData = JSON.parse(data) || []; // parse the data from string to javascript array
-    console.log(usersData);
+    getUsers();
 
-    setUsers(usersData); // set the users state with the data from local storage
+    async function getUsers() {
+      const data = localStorage.getItem("users"); // get data from local storage
+
+      let usersData = [];
+
+      if (data) {
+        // if data exists in local storage
+        usersData = JSON.parse(data); // parse the data from string to javascript array
+      } else {
+        // if data does not exist in local storage fetch the data from the API
+        usersData = await fetchUsers(); // fetch the data from the API
+      }
+
+      console.log(usersData);
+      setUsers(usersData); // set the users state with the data from local storage
+    }
   }, []);
 
-  // async function fetchUsers() {
-  //   const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json");
-  //   const data = await response.json();
-  //   setUsers(data);
-  // }
+  async function fetchUsers() {
+    const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json"); // fetch the data from the API
+    const data = await response.json(); // parse the data from string to javascript array
+    localStorage.setItem("users", JSON.stringify(data)); // save the data to local storage
+    return data; // return the data
+  }
 
   return (
     <div className="page">
